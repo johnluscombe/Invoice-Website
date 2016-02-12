a8456cdf788b225cc138b8a94793bc218806acf1
John Luscombe <johnluscombe@johns-mbp.campus.tayloru.edu>
2016-02-12 17:24:51 -0500

    postgresql2

--- db/migrate/20160122195946_create_invoices.rb
+++ db/migrate/20160122195946_create_invoices.rb
@@ -4,7 +4,8 @@ class CreateInvoices < ActiveRecord::Migration
       t.date :start_date
       t.date :end_date
       t.string :status
-      t.references :user_id, index: true, foreign_key: true
+      #t.references :user_id, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
