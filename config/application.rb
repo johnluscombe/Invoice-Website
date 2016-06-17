cf8373952c5885d3caa1da877bc539e4d302bba0
johnluscombe <john_luscombe@taylor.edu>
2016-06-16 23:00:14 -0400

    Temporary debugging change

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module Untitled
     config.active_record.raise_in_transactional_callbacks = true
     config.time_zone = 'Eastern Time (US & Canada)'
     config.active_record.default_timezone = 'Eastern Time (US & Canada)'
+
+    config.consider_all_requests_local = true
   end
 end
